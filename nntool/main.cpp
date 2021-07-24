5d911edb82cdb2525a7c451c74ec451d6a8091e3
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-07-24 18:12:42 +0300

    added windows exceptions

--- nntool/main.cpp
+++ nntool/main.cpp
@@ -6,21 +6,17 @@
 
 
 int main() {
-	NeuralNetwork nn {{2, 10, 10, 10, 10, 1}};
+	NeuralNetwork nn {{2, 2, 1}};
 	srand(time(nullptr));
 
-	for (size_t i {0}; i < 10000; ++i) {
+	for (size_t i {0}; i < 100000; ++i) {
 		float a {static_cast<float>(rand() % 10)};
 		float b {static_cast<float>(rand() % 10)};
 
 		std::vector<float> input {a, b};
 		std::vector<float> output {3 * (a + 2) + 2 * (b + 8) + 5};
 
-		nn.propagateBackwards(input, output, 0.001);
-
-		std::cout << "f(" << a << ", " << b << ") = " << 3 * (a + 2) + 2 * (b + 8) + 5
-		          << " (predicted: " << nn.feedforward(input)[0] << ", loss: "
-		          << NeuralNetwork::error(Matrix {nn.feedforward(input)}, Matrix {output}) << ")" << std::endl;
+		nn.propagateBackwards(input, output);
 	}
 
 	for (size_t i {0}; i < 50; ++i) {
@@ -29,6 +25,10 @@ int main() {
 
 		std::vector<float> input {a, b};
 		std::vector<float> output {3 * (a + 2) + 2 * (b + 8) + 5};
+
+		std::cout << "f(" << a << ", " << b << ") = " << 3 * (a + 2) + 2 * (b + 8) + 5
+		          << " (predicted: " << nn.feedforward(input)[0] << ", loss: "
+		          << NeuralNetwork::error(Matrix {nn.feedforward(input)}, Matrix {output}) << ")" << std::endl;
 	}
 
 	return 0;
