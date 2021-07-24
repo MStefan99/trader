5ec3f0ba9d471fb69b28ce519849811ac27ecd81
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-07-24 18:50:02 +0300

    changed example to use floats

--- nntool/main.cpp
+++ nntool/main.cpp
@@ -5,28 +5,35 @@
 #include "NeuralNetwork.h"
 
 
+float getResult(float a, float b) {
+	return (a - 7) / 2 + 3 * (b + 11) - 13;
+}
+
+
 int main() {
-	NeuralNetwork nn {{2, 2, 1}};
-	srand(time(nullptr));
+	NeuralNetwork nn {{2, 1}};
+	std::random_device randomDevice;
+	std::mt19937 generator(randomDevice());
+	std::uniform_real_distribution<float> distribution {-10, 10};
 
 	for (size_t i {0}; i < 100000; ++i) {
-		float a {static_cast<float>(rand() % 10)};
-		float b {static_cast<float>(rand() % 10)};
+		float a {distribution(generator)};
+		float b {distribution(generator)};
 
 		std::vector<float> input {a, b};
-		std::vector<float> output {3 * (a + 2) + 2 * (b + 8) + 5};
+		std::vector<float> output {getResult(a, b)};
 
-		nn.propagateBackwards(input, output);
+		nn.propagateBackwards(input, output, 0.01);
 	}
 
-	for (size_t i {0}; i < 50; ++i) {
-		float a {static_cast<float>(rand() % 10)};
-		float b {static_cast<float>(rand() % 10)};
+	for (size_t i {0}; i < 20; ++i) {
+		float a {distribution(generator)};
+		float b {distribution(generator)};
 
 		std::vector<float> input {a, b};
-		std::vector<float> output {3 * (a + 2) + 2 * (b + 8) + 5};
+		std::vector<float> output {getResult(a, b)};
 
-		std::cout << "f(" << a << ", " << b << ") = " << 3 * (a + 2) + 2 * (b + 8) + 5
+		std::cout << "f(" << a << ", " << b << ") = " << getResult(a, b)
 		          << " (predicted: " << nn.feedforward(input)[0] << ", loss: "
 		          << NeuralNetwork::error(Matrix {nn.feedforward(input)}, Matrix {output}) << ")" << std::endl;
 	}
