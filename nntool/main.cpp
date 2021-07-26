e1e6e993ad89eed543931f5a99d8b60fde5cd7a1
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-07-26 12:17:17 +0300

    larger values test

--- nntool/main.cpp
+++ nntool/main.cpp
@@ -14,23 +14,22 @@ int main() {
 	NeuralNetwork nn {{2, 1}};
 	std::random_device randomDevice;
 	std::mt19937 generator(randomDevice());
-	std::uniform_real_distribution<float> distribution {-10, 10};
+	std::uniform_real_distribution<float> distribution {-100, 100};
 
 	std::vector<std::vector<float>> inputs {};
 	std::vector<std::vector<float>> outputs {};
-	for (size_t i {0}; i < 12; ++i) {
+	for (size_t i {0}; i < 100000; ++i) {
 		float a {distribution(generator)};
 		float b {distribution(generator)};
 		float res {getResult(a, b)};
 
 		inputs.push_back({a, b});
 		outputs.push_back({res});
-
-		std::cout << "f(" << a << ", " << b << ") = " << res << std::endl;
 	}
 
 	std::cout << "Data ready!" << std::endl;
-	nn.fastTrain(inputs, outputs, 0.01, 100000);
+	nn.train(inputs, outputs, 0.0001);
+	// The larger the input values, the lower eta should be to avoid overshooting during training
 
 	size_t attempts {100};
 	float errorSum {};
