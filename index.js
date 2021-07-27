613a10723a405fdeba5af4481f3d40e24b39c13b
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-07-27 15:10:19 +0300

    added express server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('Hello!');
+const express = require('express');
+const app = express();
+
+
+const DEFAULT_PORT = 3000;
+
+
+app.get('/', (req, res) => {
+	res.send('Hello!');
+});
+
+
+app.listen(process.env.PORT ?? DEFAULT_PORT, () => {
+	console.log('Listening on port', process.env.PORT ?? DEFAULT_PORT);
+});
